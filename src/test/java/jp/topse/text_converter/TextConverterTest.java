627bfb813eee75da6335a00686fd0777480b223f
Takuo Doi <doi@fr-soft.com>
2024-11-18 17:02:17 +0900

    Initial Commit

---
+++ src/test/java/jp/topse/text_converter/TextConverterTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class TextConverterTest {
+    @Test
+    public void testSomething()
+    {
+        assertThat(1, is(1));
+    }
+}
